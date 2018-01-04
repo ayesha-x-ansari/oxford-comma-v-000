7f8db3e48fc789f5124e64d730d0fcbebf3062d8
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 23:02:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     puts "dddd"
     return "#{array[0]} and #{array[1]}"
 #  elsif 2 < array.length
-  elsif
+  else
     puts array.length
     array[-1].insert(1, "and ")
   end
