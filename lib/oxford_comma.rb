da9c9c6a1a3005c6e67ae0c9347153f811801dd7
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 23:14:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,10 @@
-#array = ["fiddleheads","okra","kohlrabi","dddddddddd"]
-#array = ["kiwi", "durian", "fffff"]
-array = ["sarah"]
+array = ["fiddleheads","okra","kohlrabi","dddddddddd"]
 def oxford_comma(array)
-  len = array.length
-  puts len
-  if array.length == 2
-    puts "dddd"
+  if array.length == 2            # when array length is 2 just add 2 between 2 elements
     return "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
-#  else
-    puts array.length
-    array[-1].insert(1, "and ")
+  elsif array.length > 2          # when length 0f array is greter than 2 only then excecute this step
+    array[-1].insert(0, "and ")   # -1 means last array insert adds and just before the last element
   end
-   arr =array.join(", ")
-  puts arr
-  puts "ffffff"
+  array.join(", ")                #  convert to string and add coma between the element 
 end
 
- oxford_comma(array)
