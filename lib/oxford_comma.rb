a6235a91df7702ca1e281bc2124947b12876366c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 23:06:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   if array.length == 2
     puts "dddd"
     return "#{array[0]} and #{array[1]}"
-#  elsif 2 < array.length
+#  elsif array.length > 2
   else
     puts array.length
     array[-1].insert(1, "and ")
