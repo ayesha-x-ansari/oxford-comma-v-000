55aaf6081d5ca1075e92ccd89744ac221381b3b8
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 22:47:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
     puts "dddd"
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
+    puts array.length.
     array[-1].insert(0, "and ")
   end
   array.join(", ")
